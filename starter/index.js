a5b69106dac441861a03159f8d8850b3b31e7164
ygbr5060 <aqibkhan5060@gmail.com>
2023-05-16 23:24:10 +0100

    starter code with an updated index.js file

---
+++ starter/index.js
@@ -0,0 +1,162 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+const team = [];
+
+function promptManager() {
+  console.log("Please enter the team manager's information:");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email address:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Office number:",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      team.push(manager);
+      promptMenu();
+    });
+}
+
+function promptEngineer() {
+  console.log("Please enter the engineer's information:");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email address:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "GitHub username:",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      team.push(engineer);
+      promptMenu();
+    });
+}
+
+function promptIntern() {
+  console.log("Please enter the intern's information:");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Employee ID:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email address:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "School:",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      team.push(intern);
+      promptMenu();
+    });
+}
+
+function promptMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "Select an option:",
+        choices: ["Add an engineer", "Add an intern", "Finish building the team"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.menu) {
+        case "Add an engineer":
+          promptEngineer();
+          break;
+        case "Add an intern":
+          promptIntern();
+          break;
+        case "Finish building the team":
+          generateTeamHTML();
+          break;
+        default:
+          break;
+      }
+    });
+}
+
+function generateTeamHTML(team) {
+    const html = render(team);
+  
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR, { recursive: true });
+    }
+  
+    fs.writeFileSync(outputPath, html);
+    console.log(`Team HTML generated successfully at ${outputPath}`);
+  }
